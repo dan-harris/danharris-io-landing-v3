3bbceea0c91cb8a758ac1a7c028270e1d0179160
Dan Harris <Dan.Harris@hfoh.com.au>
2019-01-06 14:13:04 +1030

    feat(bg-image): added bg image with fadein + babel setup

--- src/scripts/on-load.js
+++ src/scripts/on-load.js
@@ -1,5 +1,11 @@
-function contentLoaded() {
-  console.log("ℹ landing-page loaded 🤘");
+async function contentLoaded() {
+  const backgroundImageFadeInModule = await import("background-image-fade-in");
+  backgroundImageFadeInModule.default(
+    ".lazy-load-bg-img",
+    600,
+    "background-size: auto; background-position: center center;",
+    "position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: -1;"
+  );
 }
 
 document.addEventListener("DOMContentLoaded", contentLoaded);
